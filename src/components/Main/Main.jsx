175f1e6e34f93fd8dfe9deaff83c5a76ddacd88a
ravidukavinda <dlravindukavinda@gmail.com>
2024-01-17 17:38:40 +0530

    Searching Place Weather Forecast

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./main.css";
 import icon from "./../../assets/images/weather_icons/01d.png";
+import forecast from "./../../assets/images/weather_icons/01n.png";
 
 const Main = () => {
   return (
@@ -46,6 +47,100 @@ const Main = () => {
 
               </ul>
             </div>
+
+          </section>
+
+          {/* Searching Place Weather Forecast*/}
+          <section className="section forecast" aria-label='forecast-label'>
+
+              <h2 className="title-2" id='forecast-label'>
+                5 Days Forecast
+              </h2>
+
+              <card className="card-lg forecast-card">
+                <ul>
+                  <li className="card-item">
+                    <div className="icon-wrapper">
+                      <img src={forecast} alt="" className="weather-icon" />
+
+                      <span className="span">
+                        <p className="title-2">25</p>
+                      </span>
+                    </div>
+
+                    <p className="label-1">17 Feb</p>
+                    <p className="label-1">Friday</p>
+                  </li>
+
+                  <li className="card-item">
+                    <div className="icon-wrapper">
+                      <img src={forecast} alt="" className="weather-icon" />
+
+                      <span className="span">
+                        <p className="title-2">25</p>
+                      </span>
+                    </div>
+
+                    <p className="label-1">17 Feb</p>
+                    <p className="label-1">Friday</p>
+                  </li>
+
+                  <li className="card-item">
+                    <div className="icon-wrapper">
+                      <img src={forecast} alt="" className="weather-icon" />
+
+                      <span className="span">
+                        <p className="title-2">25</p>
+                      </span>
+                    </div>
+
+                    <p className="label-1">17 Feb</p>
+                    <p className="label-1">Friday</p>
+                  </li>
+
+                  <li className="card-item">
+                    <div className="icon-wrapper">
+                      <img src={forecast} alt="" className="weather-icon" />
+
+                      <span className="span">
+                        <p className="title-2">25</p>
+                      </span>
+                    </div>
+
+                    <p className="label-1">17 Feb</p>
+                    <p className="label-1">Friday</p>
+                  </li>
+
+                  <li className="card-item">
+                    <div className="icon-wrapper">
+                      <img src={forecast} alt="" className="weather-icon" />
+
+                      <span className="span">
+                        <p className="title-2">25</p>
+                      </span>
+                    </div>
+
+                    <p className="label-1">17 Feb</p>
+                    <p className="label-1">Friday</p>
+                  </li>
+
+                  <li className="card-item">
+                    <div className="icon-wrapper">
+                      <img src={forecast} alt="" className="weather-icon" />
+
+                      <span className="span">
+                        <p className="title-2">25</p>
+                      </span>
+                    </div>
+
+                    <p className="label-1">17 Feb</p>
+                    <p className="label-1">Friday</p>
+                  </li>
+                  
+                </ul>
+              </card>
+
+
 
           </section>
         </div>
