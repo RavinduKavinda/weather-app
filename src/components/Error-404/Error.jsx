262232139a2dc64f0974f81f5bd2630ce5bfff06
ravindukavinda <dlravindukavinda@gmail.com>
2024-01-27 18:25:29 +0530

    error 404 section added

---
+++ src/components/Error-404/Error.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './error.css'
+
+const Error = () => {
+  return (
+    <section className="error-content">
+        <h2 className="heading">
+            404
+        </h2>
+
+        <p className="body-1">
+            Page Not Found!
+        </p>
+
+        <a href="" className='btn-primary'>
+            <span className='span'>Go Home</span>
+        </a>
+    </section>
+  )
+}
+
+export default Error
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Main from '../components/Main/Main';
+import Error from '../components/Error-404/Error';
 
 
 function Layout() {
   return (<>
     <Header/>
     <Main/>
+    <Error/>
     </>
   );
 }
